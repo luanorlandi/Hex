b4db23eeb3804937478f0acccb6aa8201fc30a1e
Didier Valdez <didiervaldez@Didiers-MacBook-Pro.local>
2017-10-10 21:35:42 -0700

    Added Spanish language. Native Speaker.

---
+++ src/file/es.lua
@@ -0,0 +1,20 @@
+language["es"] = {
+    name = "Spanish",
+
+    startGame = "Comenzar nuevo juego",
+
+    welcome = "Hex es un juego de mesa para dos personas.\n\
+Cada jugador tiene un color, azul o rojo. Cada borde del tablero tiene estos colores.\n\
+El objetivo es crear un camino con tu color para conectar los bordes de tu color, a la vez evitando que tu oponente haga lo mismo.\n\
+Cada turno se te permitirá seleccionar una casilla que no esté seleccionada, la cual será de color blanco.",
+
+    blueWins = "¡Jugador Azul Ganó!",
+    redWins = "¡Jugador Rojo Ganó!",
+
+    menu = "Tamaño del Tablero: \n \
+\n \
+\n \
+Modo de juego: \n \
+\n \
+Jugador que inicia:"
+}
