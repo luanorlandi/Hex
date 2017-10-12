bd653d7b22982ce71a2a03c93b161da6b1a9f27f
César A. Vargas-Pérez <ca.vargas28@gmail.com>
2017-10-12 08:30:34 -0400

    Updating es.lua
    Correcting some translation errors, like the name that was in English, etc.

--- src/file/es.lua
+++ src/file/es.lua
@@ -1,12 +1,12 @@
 language["es"] = {
-    name = "Spanish",
+    name = "Español",
 
     startGame = "Comenzar nuevo juego",
 
-    welcome = "Hex es un juego de mesa para dos personas.\n\
-Cada jugador tiene un color, azul o rojo. Cada borde del tablero tiene estos colores.\n\
-El objetivo es crear un camino con tu color para conectar los bordes de tu color, a la vez evitando que tu oponente haga lo mismo.\n\
-Cada turno se te permitirá seleccionar una casilla que no esté seleccionada, la cual será de color blanco.",
+    welcome = "Hex es un juego de mesa para dos jugadores.\n\
+Cada jugador tiene un color, azul o rojo. El borde del tablero tiene estos colores.\n\
+El objetivo es crear un camino con tu color para conectar los bordes de tu color, evitando a la vez que tu oponente haga lo mismo.\n\
+Cada turno se te permitirá seleccionar una casilla que todavía no esté seleccionada, la cual será de color blanco.",
 
     blueWins = "¡Jugador Azul Ganó!",
     redWins = "¡Jugador Rojo Ganó!",
