6af9b99146fdae91d5656b95462fcab29d527dbe
Lukáš Hruška <lukyhruska96@gmail.com>
2017-10-26 18:14:15 +0200

    Added Czech language translation

---
+++ src/file/cs.lua
@@ -0,0 +1,20 @@
+language["cs"] = {
+    name = "Čeština",
+
+    startGame = "Začít novou hru",
+
+    welcome = "Hex je desková hra pro dva hráče.\n\
+Každý hráč má svou barvu, modrou nebo červenou. Hrany desek mají tuto barvu.\n\
+Cílem hry je vytvořit cestu tvé barvy k propojení hran tvé barvy na desce, zatímco tvůj protihráč dělá to samé.\n\
+Každý tah ti povolí si vybrat jeden hexagon, který ještě nebyl vybrát a je bilý.",
+
+    blueWins = "Modrý hráč vyhrál!",
+    redWins = "Červený hráč vyhrál!",
+
+    menu = "Velikost desky: \n \
+\n \
+\n \
+Režim hry: \n \
+\n \
+Začínající hráč:"
+}
